a7059c66c7d787023773d2fa319d213937ed3b60
geekscrapy <github.7rhdeqzfu.7@pkt5.com>
2023-09-05 11:19:48 +0400

    Update scroll_fix.py

--- scroll_fix.py
+++ scroll_fix.py
@@ -1,6 +1,8 @@
-__author__ = 'Geekscrapy'
-__version__ = '1.0'
-__description__ = '''
+'''
+Enables two functions:
+1. scrolloff: determines the number of context lines you would like to see above and below the cursor. The following command scrolls the text so that (when possible) there are always at least five lines visible above the cursor, and five lines visible below the cursor
+2. scrollfix: Lock the cursor to a particular row number in the view
+
 Allows the user to fix the position on the page where they would like the
 cursor to "stick". Helps to provide context about surrounding rows when
 near the top and bottom of the page.
@@ -8,27 +10,39 @@ near the top and bottom of the page.
 Usage: import this .py into .visidatarc, open a sheet, scroll a few
 lines down, press "w" and scroll again!
 
-Idea birthed here: https://github.com/saulpw/visidata/issues/561
+NOTE:
+    - scrollfix mode disables use off scroll-middle command
 '''
-from visidata import option, options, status, Sheet
 
-option(name='scroll_fix_enabled', default=False, helpstr='toggle scroll fix')
+__author__ = 'Geekscrapy'
+__version__ = '1.2'
+
+from visidata import option, Sheet, ALT
+
 
 @Sheet.api
-def toggle_scroll_fix(sheet):
+def checkCursor(sheet):
+    checkCursor.__wrapped__(sheet)
 
-    # Disable scroll fix
-    if options.scroll_fix_enabled:
-        options.scroll_fix_enabled = False; status('scroll fix disabled')
+    disp_scrolloff = sheet.options.disp_scrolloff
+    if disp_scrolloff:
+        if disp_scrolloff >= int(sheet.nScreenRows/2):
+            if sheet.cursorRowIndex+1 > int(sheet.nScreenRows/2):
+                sheet.topRowIndex = sheet.cursorRowIndex - int(sheet.nScreenRows/2)
+        else:
+            if sheet.cursorRowIndex-sheet.topRowIndex < disp_scrolloff:
+                sheet.topRowIndex = max(0, sheet.cursorRowIndex-sheet.options.disp_scrolloff)
 
-        Sheet.addCommand(None, 'go-down',  'cursorDown(+1)', 'go down')
-        Sheet.addCommand(None, 'go-up',    'cursorDown(-1)', 'go up')
+            if sheet.bottomRowIndex-sheet.cursorRowIndex < disp_scrolloff:
+                sheet.bottomRowIndex = sheet.cursorRowIndex+disp_scrolloff
 
-    # Enable scrollfix
-    else:
-        options.scroll_fix_enabled = True; status('scroll fix enabled')
+    if hasattr(sheet, 'disp_scrollfix') and sheet.disp_scrollfix > 0:
+        cursorViewIndex = sheet.cursorRowIndex - sheet.topRowIndex
+        if cursorViewIndex > sheet.disp_scrollfix:
+            sheet.topRowIndex += abs(sheet.disp_scrollfix - cursorViewIndex)
+        if cursorViewIndex < sheet.disp_scrollfix and sheet.topRowIndex > 0:
+            sheet.topRowIndex -= abs(sheet.disp_scrollfix - cursorViewIndex)
 
-        Sheet.addCommand(None, 'go-up', 'sheet.topRowIndex -= 1; sheet.cursorRowIndex -= 1', 'move cursor up with a fixed position')
-        Sheet.addCommand(None, 'go-down', 'sheet.topRowIndex = sheet.nRows - sheet.nScreenRows if sheet.topRowIndex + sheet.nScreenRows >= sheet.nRows else sheet.topRowIndex + 1; sheet.cursorRowIndex += 1', 'move cursor down with a fixed position')
+option(name='disp_scrolloff', default=5, helpstr='context lines to display above/below cursor when scrolling')
 
-Sheet.addCommand('w', 'scroll-fix-toggle', 'toggle_scroll_fix()', helpstr='toggle scroll fix behaviour')
+Sheet.addCommand(ALT+'w', 'scrollfix-toggle', 'sheet.disp_scrollfix, _ = (-1, status("unlocked")) if hasattr(sheet, "disp_scrollfix") and sheet.disp_scrollfix>0 else (cursorRowIndex - topRowIndex, status("locked"))', helpstr='toggle scrollfix behaviour. lock cursor view to a static row number')
